9b3c7a6243057b9184e78a87fe56526fa0072f23
RosettoA <antony.rosetto@hotmail.fr>
2023-03-17 15:17:59 +0100

    End of Section 3 : DTO et data management

---
+++ NZWalks.Api/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.Api.Data;
+using NZWalks.Api.Models.Domain;
+
+namespace NZWalks.Api.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext _dbContext;
+        public RegionRepository(NZWalksDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+            return await _dbContext.Regions.ToListAsync();
+        }
+    }
+}
